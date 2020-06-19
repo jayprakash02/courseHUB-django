e83acd7cbcae4da5361c23959d775d574bb54622
Jay Prakash <50571500+jayprakash02@users.noreply.github.com>
2020-06-19 23:26:04 +0530

    all product page is made

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path,include
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index , name='main'),
 ] 
--- products/views.py
+++ products/views.py
@@ -3,4 +3,4 @@ from .models import Allproduct
 # Create your views here.
 def allProducts(request):
     product_list = Allproduct.objects.all()        
-    return render(request,'product.html',{'product_list':product_list})
+    return render(request,'allproduct.html',{'product_list':product_list})
